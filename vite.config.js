1d3b9e55e4b7622bb896a82919554026087fcfbc
jaredhortiales <contacto@jaredhortiales.com>
2026-02-24 11:48:40 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // Cambie "sistemaprl-frontend-staging" por el nombre exacto de su repo:
-  base: "/sistemaprl-frontend-staging/"
+  base: "/sistemaprl/"
+
 });
